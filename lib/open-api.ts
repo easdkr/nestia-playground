18f2375acc42d5c4e4c803d23068235ccf6bd529
June <easdkr@daum.net>
2024-03-19 20:00:03 +0900

    [chore] open api 설정 lib 쪽으로 변경
    - spec 서버 라우팅 설정 옵셔널하게 변경
    - 로깅 추가

--- lib/open-api.ts
+++ lib/open-api.ts
@@ -1,11 +1,24 @@
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, Logger } from '@nestjs/common';
 import { SwaggerModule } from '@nestjs/swagger';
+import { execSync } from 'child_process';
 import { readFileSync } from 'fs';
 import * as path from 'path';
 
-export function setOpenApi(app: INestApplication): void {
+type Option = {
+  runOnApplicationBootstrap?: boolean;
+};
+
+export function setOpenApi(app: INestApplication, option?: Option): void {
+  const logger = new Logger('OpenAPI');
+  execSync('npx nestia swagger');
+  logger.verbose('OpenAPI spec is generated');
+
+  if (!option?.runOnApplicationBootstrap) return;
+
   const swagger = readFileSync(path.join(__dirname, '../swagger.json'), 'utf8');
   const document = JSON.parse(swagger);
 
   SwaggerModule.setup('api/docs', app, document);
+
+  logger.verbose('OpenAPI is available at http://localhost:3000/api/docs');
 }
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setOpenApi } from '../lib';
-import { execSync } from 'child_process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  execSync('npx nestia swagger');
-  console.log('OpenAPI is available at http://localhost:3000/api/docs');
-  setOpenApi(app);
+  setOpenApi(app, { runOnApplicationBootstrap: true });
   await app.listen(3000);
 }
 bootstrap();
