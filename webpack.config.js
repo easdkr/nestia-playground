39d24e4571ab6217d80b604733abdc593e2285a5
June <easdkr@daum.net>
2024-03-21 09:03:05 +0900

    [WIP] webpack 빌드 추가

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  // CUSTOMIZE HERE
+  entry: {
+    server: './apps/api/src/main.ts',
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  optimization: {
+    minimize: true,
+  },
+  // JUST KEEP THEM
+  externals: [nodeExternals()],
+  mode: 'development',
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: [/node_modules/],
+        loader: 'ts-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    alias: {
+      '@api': path.resolve(__dirname, 'apps/api/src'),
+      '@common/configs': path.resolve(__dirname, 'libs/configs/src'),
+      '@common/prisma': path.resolve(__dirname, 'libs/prisma/src'),
+    },
+  },
+};
