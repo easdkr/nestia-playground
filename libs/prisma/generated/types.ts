08c20313741c56dee57f93312af56bf4fa5f6194
June <easdkr@daum.net>
2024-03-24 17:28:00 +0900

    [feat] user 테이블 정의 추가

--- libs/prisma/generated/types.ts
+++ libs/prisma/generated/types.ts
@@ -1,15 +1,38 @@
-import type { ColumnType } from "kysely";
-export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
-  ? ColumnType<S, I | undefined, U>
-  : ColumnType<T, T | undefined, T>;
+import type { ColumnType } from 'kysely';
+export type Generated<T> =
+  T extends ColumnType<infer S, infer I, infer U>
+    ? ColumnType<S, I | undefined, U>
+    : ColumnType<T, T | undefined, T>;
 export type Timestamp = ColumnType<Date, Date | string, Date | string>;
 
+export type Password = {
+  id: Generated<string>;
+  user_id: string;
+  password: string;
+  created_at: Generated<Timestamp>;
+  updated_at: Timestamp;
+  deleted_at: Timestamp | null;
+};
+export type Profile = {
+  id: Generated<string>;
+  user_id: string;
+  first_name: string;
+  middle_name: string | null;
+  last_name: string;
+  created_at: Generated<Timestamp>;
+  updated_at: Timestamp;
+  deleted_at: Timestamp | null;
+};
 export type User = {
-    id: string;
-    email: string;
-    name: string | null;
-    password: string;
+  id: Generated<string>;
+  email: string;
+  authentication_type: Generated<string>;
+  created_at: Generated<Timestamp>;
+  updated_at: Timestamp;
+  deleted_at: Timestamp | null;
 };
 export type DB = {
-    User: User;
+  _users: User;
+  passwords: Password;
+  profiles: Profile;
 };
---
+++ libs/prisma/migrations/20240324081608_wpqkf/migration.sql
@@ -0,0 +1,52 @@
+-- CreateTable
+CREATE TABLE "_users" (
+    "id" BIGSERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "authentication_type" TEXT NOT NULL DEFAULT 'email',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "_users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "profiles" (
+    "id" BIGSERIAL NOT NULL,
+    "user_id" BIGINT NOT NULL,
+    "first_name" TEXT NOT NULL,
+    "middle_name" TEXT,
+    "last_name" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "profiles_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "passwords" (
+    "id" BIGSERIAL NOT NULL,
+    "user_id" BIGINT NOT NULL,
+    "password" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "passwords_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_users_email_key" ON "_users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "profiles_user_id_key" ON "profiles"("user_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "passwords_user_id_key" ON "passwords"("user_id");
+
+-- AddForeignKey
+ALTER TABLE "profiles" ADD CONSTRAINT "profiles_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "_users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "passwords" ADD CONSTRAINT "passwords_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "_users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
